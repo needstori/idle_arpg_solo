c3d4b7beb0d9fc2fd396a0a35153e864afffcb86
Tim <bloobluu@hotmail.com>
2021-09-20 00:26:01 +1200

    Added some item storage and the ability to swap the item the player is holding with an item in storage.

--- src/item.py
+++ src/item.py
@@ -4,3 +4,5 @@ from stat_holder import StatHolder
 class Item:
     def __init__(self):
         self.stats = StatHolder()
+
+        self.name = "Item Name"
--- src/main.py
+++ src/main.py
@@ -7,12 +7,18 @@ from kivy.uix.label import Label
 from kivy.properties import (NumericProperty, ObjectProperty, StringProperty)
 from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+from kivy.graphics import Color, Rectangle
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.behaviors import ButtonBehavior
 
 from character import Character
 from stats import Stats
 from stat_holder import StatHolder
 from world_area import WorldArea
 from item import Item
+
+from functools import partial
 
 
 seed(time.time())
@@ -88,6 +94,7 @@ class WorldAreaDisplay(Label):
         self.defeatedMonsters = defeated_monsters
         self.totalMonsters = total_monsters
 
+
 class GameScreen(Screen):
     def __init__(self, game, **kwargs):
         super().__init__(**kwargs)
@@ -122,11 +129,13 @@ class BattleScreen(GameScreen):
 class EmbarkScreen(GameScreen):
     player_display = ObjectProperty(None)
     start_button = ObjectProperty(None)
+    inventory_button = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         self.start_button.bind(on_press=self.on_start_press)
+        self.inventory_button.bind(on_press=self.on_inventory_press)
 
     def update_display(self, dt):
         super().update_display(dt)
@@ -137,10 +146,86 @@ class EmbarkScreen(GameScreen):
         self.game.embark()
         self.manager.current = "battle_screen"
 
+    def on_inventory_press(self, instance):
+        self.game.state_transition(GameState.INVENTORY)
+        self.manager.current = "inventory_screen"
+
+
+class GridButton(ButtonBehavior, GridLayout):
+    pass
+
+
+class ItemLabel(ButtonBehavior, Label):
+    pass
+
+
+class ItemList(RecycleView):
+    def __init__(self, **kwargs):
+        super(ItemList, self).__init__(**kwargs)
+
+
+class InventoryScreen(GameScreen):
+    current_item_text = StringProperty("")
+    player_display = ObjectProperty(None)
+    player_item = Label()
+    player_item_name = StringProperty("")
+    item_list = ObjectProperty(None)
+    navigation_embark = ObjectProperty(None)
+    equip_button = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.selected_item = None
+
+        self.player_item.bind(on_press=self.show_player_items)
+        self.navigation_embark.bind(on_press=self.back_to_embark)
+        self.equip_button.bind(on_press=self.equip_selected)
+
+    def display_item_from_list(self, index):
+        self.selected_item = self.game.item_storage[index]
+        self.update_display(0)
+
+    def update_display(self, dt):
+        super().update_display(dt)
+        if self.game:
+            if self.selected_item:
+                item_text = self.selected_item.name + "\n"
+                for stat_name, stat_value in self.selected_item.stats.get_existing_stats():
+                    item_text += f"{stat_name}: {stat_value}\n"
+                self.current_item_text = item_text
+            if self.player_display:
+                self.player_display.update_character_display(self.game.player.stats)
+            if self.game.player.currentItem:
+                self.player_item_name = self.game.player.currentItem.name
+
+            self.item_list.data = [{'text': item.name, 'on_press': partial(self.display_item_from_list, index)}
+                                   for index, item in enumerate(self.game.item_storage)]
+
+    def show_player_items(self, press):
+        self.selected_item = self.game.player.currentItem
+        self.update_display(0)
+
+    def equip_selected(self, press):
+        if self.selected_item is None or self.game.player.currentItem == self.selected_item:
+            return
+
+        # Remove the selected item from storage and add it to the player
+        new_player_item = self.selected_item
+        self.game.item_storage.remove(new_player_item)
+        current_player_item = self.game.player.equip_item(new_player_item)
+        self.game.item_storage.append(current_player_item)
+
+        self.update_display(0)
+
+    def back_to_embark(self, press):
+        self.game.state_transition(GameState.AT_BASE)
+        self.manager.current = "embark_screen"
+
 
 class GameState:
     AT_BASE = 'at_base'
     COMBAT = 'combat'
+    INVENTORY = 'inventory'
 
 
 class IdleArpgGame:
@@ -150,6 +235,12 @@ class IdleArpgGame:
         self.current_monster = None
 
         self.current_state = GameState.AT_BASE
+        self.item_storage = []
+        for i in range(10):
+            new_item = Item()
+            new_item.stats.add_stat(Stats.MAXIMUM_HEALTH, i)
+            new_item.name = f"Item {i}"
+            self.item_storage.append(new_item)
 
     def create_player(self):
         # Create the player
@@ -157,6 +248,11 @@ class IdleArpgGame:
         self.player.stats.add_stat(Stats.MIN_DAMAGE, 1)
         self.player.stats.add_stat(Stats.MAX_DAMAGE, 3)
         self.player.stats.add_stat(Stats.DEFENCE, 2)
+
+        # Add an item to the player
+        new_item = Item()
+        new_item.stats.add_stat(Stats.MAXIMUM_HEALTH, 5)
+        self.player.equip_item(new_item)
 
     def create_world_area(self):
         # Create a world area
@@ -205,7 +301,6 @@ class IdleArpgGame:
             pass
 
 
-
 class IdleArpgApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -221,20 +316,24 @@ class IdleArpgApp(App):
         self.screen_manager.current_screen.update_display(dt)
 
     def build(self):
-
         # Create the game state
         self.game = IdleArpgGame()
         self.game.create_player()
 
         self.screen_manager = ScreenManager()
         battle_screen = BattleScreen(game=self.game, name="battle_screen")
-        # battle_screen.update_display()
         self.screen_manager.add_widget(battle_screen)
 
         embark_screen = EmbarkScreen(game=self.game, name="embark_screen")
         self.screen_manager.add_widget(embark_screen)
+        self.game.state_transition(GameState.AT_BASE)
         self.screen_manager.current = "embark_screen"
         embark_screen.update_display(0)
+
+        inventory_screen = InventoryScreen(game=self.game, name="inventory_screen")
+        self.screen_manager.add_widget(inventory_screen)
+        # self.game.state_transition(GameState.INVENTORY)
+        # self.screen_manager.current = "inventory_screen"
 
         # Setup the game clock and display clock
         Clock.schedule_interval(self.game.update, 1)
